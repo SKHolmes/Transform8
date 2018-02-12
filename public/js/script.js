d8084848215da5ec3b421406ccefd84079f9a8ba
Sam holmes <samualkholmes@gmail.com>
2018-02-12 14:44:19 +1300

    Committing to get GIT fully up to date. No real changes.

--- public/js/script.js
+++ public/js/script.js
@@ -305,20 +305,4 @@
 			right.removeChild(right.firstChild);
 		}
 	}
-
-	/*The fuck was I doing here? Works tho
-
-	function loadXMLDoc() {
- 		var xhttp = new XMLHttpRequest();
-  		xhttp.onreadystatechange = function() {
-    	if (this.readyState == 4 && this.status == 200) {
-      		console.log(this.responseText);
-    		}
-  		};
-  		xhttp.open("GET", "test.txt", true);
-  		xhttp.send();
-  	}
-
-  	loadXMLDoc();*/
-
   }());
